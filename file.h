9999ae67c460fe903e7f1e54756f3abed531ef1b
razuvayev-d <46049860+razuvayev-d@users.noreply.github.com>
2018-12-27 11:06:39 +0300

    Условно завершенная версия
    Ориентировочные сведения по задачам классов: 
    Класс kernel -- преобразует исходное число в зашифрованную пару и наоборот. 
    Класс shell -- преобразует исходную строку в пару целочисленных векторов и наоборот используя возможности класса kernel
    Класс wwfile -- абстрактный класс для работы с файлами
    Класс file -- реализует конкретные функции для работы с файлами в рамках поставленной задачи, наследник класса wwfile
    Класс ElGamal -- преобразует исходный файл в пару файлов-шифров и пару файлов-ключей. Использует возможности классов shell и file.

---
+++ file.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "wwfile.h"
+
+/*Абстрактный класс, реализующий основные функции для работы с файлами
+* необходимые классу ElGamal
+*/
+class file : private wwfile {
+public:
+	/*Проверяет существует ли файл по такому адресу
+    * Требует от пользователя ввести корректный адрес до тех пор, 
+	* пока тот не сдастся
+    */
+	static string ExCheck(string Adress){
+		ifstream file(Adress);
+		while (true) {
+			if (!file) {
+				file.close();
+				cout << "Файл с адресом " << Adress << " не открыт.";
+			}
+			else break;
+			cout << " Введите адрес файла... ";
+			cin >> Adress;
+			file.open(Adress);
+		}
+		return Adress;
+	}
+	/*Чтение файла (для открытого текста)*/
+	string ReadFile(const string FileAdress);
+	/*Запись в файл массива чисел
+	* @param Cryp -- массив чисел
+	* @param FileAdress -- адрес файла
+	*/
+	void WriteInFile(const vector<size_t> Cryp, const string FileAdress);
+	
+	/*Запись в файл текста*/
+	void WriteInFile(const string Text, const string FileAdress);
+	/*Чтение зашифрованных файлов
+	* @param FileAdress -- адрес файла, который необходимо прочитать
+	* возвращает массив целых положительных чисел
+	*/
+	vector<size_t> ReadCryptFile(const string FileAdress);
+
+	/*Функция для создания файла*/
+	void Createfile(const string FileAdress);
+ };
---
+++ main.cpp
@@ -0,0 +1,67 @@
+﻿#include "kernel.h"
+#include "file.h"
+#include "ElGamal.h"
+
+/*Демонстрация работы и общение с пользователем через консоль*/
+	void Encryption();
+	void Decryption();
+
+	int main() {
+		setlocale(LC_ALL, "RUSSIAN");
+		int g;
+		cout << "Выберите режим работы 1 -- шифрование, 0 -- расшифрование" << endl;
+		cin >> g;
+		switch (g)
+		{
+		case 1: Encryption(); break;
+		case 0: Decryption(); break;
+		default:
+			cout << "Такого режима не существует";
+			break;
+		}
+	}
+
+	void Encryption() {
+		int g; string FileAdr;
+		cout << "Чтобы ввести адрес файла с открытым текстом нажмите 1. Чтобы использовать адрес по-умолчанию нажмите 0";
+		cin >> g;
+		if (g) {
+			cout << "Введит адрес файла с открытым текстом: " << endl;
+			cin >> FileAdr; cout << endl;
+			ElGamal M(FileAdr);
+			M.Encryption();
+			cout << "Файл был зашифрован." << endl
+				<< "В каталоге нахождения исходного файла созданы файлы:" << endl
+				<< "CryptoTextA.txt и CryptoTextB.txt -- зашифрованное сообщение" << endl
+				<< "PublicKey.txt и PrivateKey.txt -- открытый и закртый ключ соответственно" << endl;
+		}
+		else {
+			ElGamal S;
+			cout << "В каталоге программы была создана папка Crypt." << endl
+				<< "Найдите в ней файл OpenText.txt и запишите туда ваше сообщение" << endl;
+			system("pause");
+			cin.get();
+			S.Encryption();
+			cout << "Файл был зашифрован." << endl
+				<< "В каталоге Crypt созданы файлы:" << endl
+				<< "CryptoTextA.txt и CryptoTextB.txt -- зашифрованное сообщение" << endl
+				<< "PublicKey.txt и PrivateKey.txt -- открытый и закртый ключ соответственно" << endl;
+			system("pause");
+		}
+	}
+	void Decryption() {
+		string A, B, Pub, Pr;
+		cout << "Введите адрес файла CryptoTextA.txt" << endl;
+		cin >> A; A = file::ExCheck(A);
+		cout << "Введите адрес файла CryptoTextB.txt" << endl;
+		cin >> B; B = file::ExCheck(B);
+		cout << "Введите адрес файла PublicKey.txt" << endl;
+		cin >> Pub; Pub = file::ExCheck(Pub);
+		cout << "Введите адрес файла PrivateKey.txt" << endl;
+		cin >> Pr; Pr = file::ExCheck(Pr);
+		ElGamal P; P.Decryption(A, B, Pub, Pr);
+		cout << "Адрес расшифровки: Crypt/Decr.txt";
+		system("pause");
+	}
+
+	
